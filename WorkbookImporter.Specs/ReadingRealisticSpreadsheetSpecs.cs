a436dc11e4b92e10fc487b02241914973616f3fc
Paul Rohorzka <paul.rohorzka@squer.io>
2025-10-21 21:53:17 +0200

    add realistic sample

---
+++ WorkbookImporter.Specs/ReadingRealisticSpreadsheetSpecs.cs
@@ -0,0 +1,62 @@
+using MiniExcelLibs.Attributes;
+using Shouldly;
+
+namespace PaulRoho.Evaluate.ReadingWritingWorkbooks.Specs;
+
+public class ReadingRealisticSpreadsheetSpecs
+{
+    private readonly SpreadsheetReader _reader;
+
+    public ReadingRealisticSpreadsheetSpecs()
+    {
+        _reader = new SpreadsheetReader();
+    }
+
+    private record RealisticRow : SpreadsheetReader.IRowMarker
+    {
+        [ExcelColumnName("Group")] public string Group { get; init; } = string.Empty;
+        [ExcelColumnName("")] public string Part { get; init; } = string.Empty;
+        [ExcelColumnName("The Text")] public string Text { get; init; } = string.Empty;
+        [ExcelColumnName("The Key")] public string Key { get; init; } = string.Empty;
+        [ExcelColumnName("The Value")] public decimal Value { get; init; }
+
+        public bool IsProcessable => !string.IsNullOrWhiteSpace(Text);
+    }
+
+    [Fact]
+    public void CanReadDataWithGrouping()
+    {
+        // Act
+        var dataFromFile = _reader.ReadSpreadsheet<RealisticRow>(
+            "SampleFiles/RealisticTemplateFilled.xlsx",
+            "Important Sheet",
+            "B12");
+
+        dataFromFile.Count.ShouldBe(3);
+
+        dataFromFile[0].ShouldBeEquivalentTo(new RealisticRow
+        {
+            Group = "Group A.a",
+            Part = "",
+            Text = "Text A.1",
+            Key = "A_a_A1",
+            Value = 1.1m
+        });
+        dataFromFile[1].ShouldBeEquivalentTo(new RealisticRow
+        {
+            Group = "",
+            Part = "",
+            Text = "Text A.2",
+            Key = "A_a_A2",
+            Value = 2.22m
+        });
+        dataFromFile[2].ShouldBeEquivalentTo(new RealisticRow
+        {
+            Group = "Group A.b",
+            Part = "",
+            Text = "Text A.3",
+            Key = "A_b_A3",
+            Value = 3.333m
+        });
+    }
+}
